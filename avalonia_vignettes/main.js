b2044eca21e1c4b9130cdd93a05c8a3c8765a40f
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2026-09-24 16:20:00 +0200

    Remove vignettes

--- avalonia_vignettes/main.js
+++
@@ -1,9 +0,0 @@
-import { dotnet } from './_framework/dotnet.js';
-
-const runtime = await dotnet
-  .withDiagnosticTracing(false)
-  .withApplicationArgumentsFromQuery()
-  .create();
-
-const config = runtime.getConfig();
-await runtime.runMain(config.mainAssemblyName, [globalThis.location.href]);
